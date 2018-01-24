7bc30b25bc2c03b509573902a04210b8277db3d7
MamoruMurakami <mamoru@prty.jp>
2018-01-25 02:45:49 +0900

    fix bug

--- src/Mpociot/ApiDoc/Generators/AbstractGenerator.php
+++ src/Mpociot/ApiDoc/Generators/AbstractGenerator.php
@@ -35,7 +35,7 @@ abstract class AbstractGenerator
      *
      * @return array
      */
-    abstract public function processRoute($route, $bindings = [], $actUserIdAs, $withResponse = true);
+    abstract public function processRoute($route, $bindings = [], $headers = [],  $actUserIdAs, $withResponse = true);
 
     /**
      * Prepares / Disables route middlewares.
